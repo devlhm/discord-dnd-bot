07c7bf975db518adec083467fd6266e34994ffd2
Luis Henrique <devlhm05@gmail.com>
2019-07-21 20:49:48 -0300

    dbObjects

--- dbObjects.js
+++ dbObjects.js
@@ -1,10 +1,11 @@
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize('fichas', null, null, {
-	host: 'localhost',
-	dialect: 'sqlite',
-	operatorAliases: false,
-	storage: 'fichas.sqlite'
+const connection = new Sequelize(process.env.DATABASE_URL, {
+	dialect: 'postgres',
+	protocol: 'postgres',
+	port: 5432,
+	host: 'ec2-54-221-215-228.compute-1.amazonaws.com',
+	operatorAliases: false
 });
 
 const Atributos = connection.import('models/Atributos');
