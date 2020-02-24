ef235a12e3122f48386058752652148b5ba220ab
abmoraes16 <a.bmoraes@outlook.com>
2020-02-23 21:35:51 -0300

    async bug fix

--- commands/setImage.js
+++ commands/setImage.js
@@ -1,7 +1,7 @@
 module.exports = {
 	name: 'definir-imagem',
 	desc: '<personagem> - Adiciona uma imagem à ficha do personagem selecionado',
-	execute (client, message, args) {
+	async execute (client, message, args) {
 		if (args.length > 1) {
 			message.channel.send('Escolha um personagem por vez!');
 			return;
